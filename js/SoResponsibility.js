e9a04a75f2dc618800d3b6768649f45c3a97c5d8
Ben Weatherall <ben.weatherall@gmail.com>
2014-09-24 10:02:35 +0930

    updated SOR to include diacritics

--- js/SoResponsibility.js
+++ js/SoResponsibility.js
@@ -4,13 +4,34 @@ $(document).ready(function() {
 			var noLines = $(this).val();
 			//REMOVE NEWLINES, TABS. REPLACE WITH SPACE. DELETE "double" SPACES
 			noLines = noLines.replace(/[\r\n\t]/g, ' ');
-			noLines = noLines.replace(/ {2,}/g, ' ');
 
 			//REMOVE NON-ROMAN NAME CHARACTERS, CLEAN EXTRA COMMARS SPACES
-			noLines = noLines.replace(/[^a-zA-Z,\s.\-']/g, '');
-			noLines = noLines.replace(/([a-zA-Z.\-]+)[\s,]*,/g, '$1,')
+			var nonRoman = /[^a-zA-Z,\s\.\-']/g;
+			var match = noLines.match(nonRoman);
+			var Notletters = XRegExp('\\p{L}', 'ig');
+			if(match) {
+				console.log(match);
+				for(var i = 0; i < match.length; i++) {
+					if (!XRegExp.test(match[i], Notletters)) {
+						var re = new RegExp(match[i], 'g');
+						noLines = noLines.replace(re, ", ");
+					}
+				}
+			}
+			noLines = noLines.replace(/ {2,}/g, ' ');
 
+			//REPLACE MULTIPLE SPACES / COMMAS WITH SINGLE
+			noLines = noLines.replace(/([a-zA-z.\-]+)[\s,]*,/g, '$1,');
+			noLines = noLines.replace(/[\s,]*,/g, ',');
+
+			//ENSURE SPACE AFTER EACH COMMA
+			noLines = noLines.replace(/,([^ ])/g, ', $1');
 			noLines = noLines.trim();
+			
+			if(noLines.charAt(noLines.length -1) == ',') {
+				noLines = noLines.substring(0, noLines.length -1);
+			}
+
 			$( "#SOROutput" ).text(noLines);
 	});
 });
